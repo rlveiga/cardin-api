a9aa69cb066c65741ae773ac94cce8fc725eda29
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2019-12-14 14:09:54 -0300

    Initial migration

--- app/models.py
+++ app/models.py
@@ -4,3 +4,5 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    email = db.Column(db.String(64), nullable=False, unique=True)
