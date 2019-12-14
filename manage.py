899512eabbcd545143b7e13eaa2f5f5e9729d2cb
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2019-12-14 01:33:47 -0300

    Creating users table

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from app import create_app, db
+from app.models import User
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand 
 import os
@@ -10,7 +11,7 @@ Migrate = Migrate(app, db)
 # initialize db migrate variable here
 
 def make_shell_context():
-	return dict(app=app, db=db)
+	return dict(app=app, db=db, User=User)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
