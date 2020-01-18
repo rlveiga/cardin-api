5331e0f63964f6c96f30a88aa491f9351d2188e1
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2020-01-18 20:13:48 -0300

    Writing tests for user registration

--- tests/integration/test_user.py
+++ tests/integration/test_user.py
@@ -2,6 +2,25 @@ import json
 
 from app.models.user import User
 
+def test_create_user(test_client, init_db):
+    response = test_client.post('/auth/register', json=dict(username='test_user', password='abc123'))
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 201
+
+    assert data['message'] == 'User created'
+    assert type(data['user']) is dict
+
+def test_create_user_fail(test_client, init_db):
+    response = test_client.post('/auth/register', json=dict(username='rodrigo', password='abc123'))
+
+    data = json.loads(response.data)
+
+    assert response.status_code == 409
+
+    assert data['message'] == 'Username is taken'
+
 def test_login_success(test_client, init_db):
     response = test_client.post('/auth/login', json=dict(username='rodrigo', password='abc123'))
     
