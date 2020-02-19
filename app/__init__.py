c6b1d8d1a4baf5d0f0a1731b27b3dd5f71133f79
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2020-02-19 02:00:04 -0300

    Setting up socket.io

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,33 @@
 from flask import Flask
+from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from config import config 
 
+import os
+
 db = SQLAlchemy()
 ma = Marshmallow()
 
-def create_app(config_name):
-	app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
-	ma.init_app(app)
-	
-	app.config.from_object(config[config_name])
-	config[config_name].init_app(app)
+ma.init_app(app)
 
-	db.init_app(app)
+app.config.from_object(config[os.getenv('FLASK_CONFIG') or 'default'])
+config[os.getenv('FLASK_CONFIG') or 'default'].init_app(app)
 
-	from .auth import auth as auth_blueprint
-	from .card import card as card_blueprint
-	from .collection import collection as collection_blueprint
-	from .room import room as room_blueprint
+# Creating SocketIO app here so that
+# it can be accessed by views
+socketio = SocketIO(app)
 
-	app.register_blueprint(auth_blueprint)
-	app.register_blueprint(card_blueprint)
-	app.register_blueprint(collection_blueprint)
-	app.register_blueprint(room_blueprint)
+db.init_app(app)
 
-	return app
+from .auth import auth as auth_blueprint
+from .card import card as card_blueprint
+from .collection import collection as collection_blueprint
+from .room import room as room_blueprint
+
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(card_blueprint)
+app.register_blueprint(collection_blueprint)
+app.register_blueprint(room_blueprint)
--- manage.py
+++ manage.py
@@ -1,20 +1,25 @@
-from app import create_app, db
+from app import app, db, socketio
 from app.models.user import User
 from app.models.room import Room, RoomAssociation
 from app.models.card import Card
 from app.models.collection import Collection
 
+import flask
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand 
+from flask_socketio import SocketIO
 
 import os
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 Migrate = Migrate(app, db)
 
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Card=Card, Collection=Collection, Room=Room, RoomAssociation=RoomAssociation)
+
+@manager.command
+def run():
+  socketio.run(app)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
