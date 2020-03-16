6515fe68b446c5e4835e4cc02e4653db97875b1d
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2020-03-15 21:06:19 -0300

    Renaming listeners

--- app/room/__init__.py
+++ app/room/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 room = Blueprint('room', __name__, url_prefix='/rooms')
 
 from . import views
-from . import socketio
+from . import game
--- app/room/game.py
+++ app/room/game.py
@@ -3,19 +3,18 @@ from flask_socketio import emit, join_room, leave_room, rooms
 
 @socketio.on('join')
 def join(data):
-    print('join')
     room_code = data['room']
     user = data['user']
 
     join_room(room_code)
 
-    emit('join_response', f"{user['username']} has joined {room_code}", room=room_code)
+    emit('join_response', {'user': user}, room=room_code)
 
 @socketio.on('leave')
-def on_leave(data):
+def leave(data):
     room_code = data['room']
     user = data['user']
 
     leave_room(room_code)
 
-    emit('leave_response', f"{user} has left {room_code}", room=room_code)
+    emit('leave_response', {'user': user}, room=room_code)
