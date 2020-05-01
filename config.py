6ed7ee03533560a264ad80df0e3f732b60cc64b9
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2020-05-01 19:45:41 -0300

    Updating database uri

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class DevelopmentConfig(Config):
 
 	# Should set environment variables for database uri in the future
 
-	SQLALCHEMY_DATABASE_URI = 'postgresql://rlveiga:@localhost/cardin_dev'
+	SQLALCHEMY_DATABASE_URI = 'postgres://uiulrhslqdnhue:1da67cc96eda4402689b73925bcb01f6c3bde1b5bf7f3c66f55acdc54ecea930@ec2-54-165-36-134.compute-1.amazonaws.com:5432/d1cqcq6m3tet7j'
 
 class TestingConfig(Config):
 	TESTING = True
