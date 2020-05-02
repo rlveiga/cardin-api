6b77bd2e716ab6e6ce219e319bdaa868a9776de9
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2020-05-02 13:25:00 -0300

    null game data bug fix

--- app/models/game.py
+++ app/models/game.py
@@ -39,13 +39,14 @@ class Game(db.Model):
     def remove_player(self, user_id):
         game_data = self.load_game_data()
 
-        for player in game_data['players']:
-            if player['data']['id'] == user_id:
-                game_data['players'].remove(player)
+        if game_data is not None:
+            for player in game_data['players']:
+                if player['data']['id'] == user_id:
+                    game_data['players'].remove(player)
 
-        if len(game_data['players']) < 3:
-            print('game ended')
-            self.end_game()
+            if len(game_data['players']) < 3:
+                print('game ended')
+                self.end_game()
 
     def start_game(self, collection_id):
         collection = Collection.query.filter_by(id=collection_id).first()
